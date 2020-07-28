421ba16d3cc580eafb340dca64d1b6a4936ad533
SenneScheepersPXL <Senne.Scheepers@student.pxl.be>
2020-07-28 16:39:32 +0200

    home page desgin klaar

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const handleLoad = () => {
+    document.getElementById("open-close").addEventListener('click', openClose);
+};
+
+const openClose = () => {
+    let button = document.getElementById("open-close");
+    let nav = document.getElementById("nav-bar");
+    let open = button.className == "fa fa-bars";
+    if (open) {
+        button.className = "fa fa-times";
+        nav.className = "active";
+    } else {
+        button.className = "fa fa-bars";
+        nav.className = "";
+    }
+};
+
+window.addEventListener('load', handleLoad);

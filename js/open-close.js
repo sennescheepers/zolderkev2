b5110143371c3b78ad99404ef458725f7482d086
SenneScheepersPXL <Senne.Scheepers@student.pxl.be>
2020-08-25 10:27:49 +0200

    over ons klaar

--- js/open-close.js
+++ js/open-close.js
@@ -1,6 +1,5 @@
 const handleLoad = () => {
     document.getElementById("open-close").addEventListener('click', openClose);
-    //window.addEventListener('scroll', changePage, false);
 }
 
 const openClose = () => {
@@ -15,21 +14,5 @@ const openClose = () => {
         nav.className = "";
     }
 };
-
-let lastScrollTop = 0;
-
-const changePage = (e) => {
-    let st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-    if (st > lastScrollTop){
-        window.scrollTo({
-            top: document.getElementById('team').offsetTop,
-            left: 0,
-            behavior: 'smooth'
-        });
-    } else {
-        console.log("up");
-    }
-    lastScrollTop = st <= 0 ? 0 : st;
-}
 
 window.addEventListener('load', handleLoad);
